bd128a118b062542c94a1523b5166afc16402aed
metal-oopa <sudip5banerjee1974@gmail.com>
2022-10-15 01:15:06 +0530

    convert date to DD-MM-YYYY format in statistics

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -5,14 +5,18 @@ export type StatisticsType = {
 
 export default function Statistics(props: StatisticsType) {
   const { questionsCount, lastUpdated } = props;
+
+  // convert the date to format DD-MM-YYYY
+  const lastUpdatedDate = new Date(lastUpdated).toLocaleDateString();
+
   return (
-    <div className='card'>
-      <header className='card-header'>
-        <p className='card-header-title'>Statistics</p>
+    <div className="card">
+      <header className="card-header">
+        <p className="card-header-title">Statistics</p>
       </header>
-      <div className='card-content'>
-        <div className='content'>Questions in DB: {String(questionsCount)}</div>
-        <div className='content'>Last updated: {lastUpdated}</div>
+      <div className="card-content">
+        <div className="content">Questions in DB: {String(questionsCount)}</div>
+        <div className="content">Last updated: {lastUpdatedDate}</div>
         {/* <div className='content'>Registered users: {String(questionsCount)}</div> */}
       </div>
     </div>

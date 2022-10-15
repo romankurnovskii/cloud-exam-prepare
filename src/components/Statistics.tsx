d899e5d1bfd38719c94b1544c2f158c30f3f506f
Roman Kurnovskii <r.kurnovskii@gmail.com>
2022-10-15 14:40:39 +0300

    add subscribe form

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -5,19 +5,23 @@ export type StatisticsType = {
 
 export default function Statistics(props: StatisticsType) {
   const { questionsCount, lastUpdated } = props;
-
-  // convert the date to format DD-MM-YYYY
-  const lastUpdatedDate = new Date(lastUpdated).toLocaleDateString();
+  const lastUpdatedDate = new Date(lastUpdated).toLocaleDateString(); // DD-MM-YYYY
 
   return (
-    <div className="card">
-      <header className="card-header">
-        <p className="card-header-title">Statistics</p>
+    <div className='card'>
+      <header className='card-header'>
+        <p className='card-header-title'>Statistics</p>
       </header>
-      <div className="card-content">
-        <div className="content">Questions in DB: {String(questionsCount)}</div>
-        <div className="content">Last updated: {lastUpdatedDate}</div>
+      <div className='card-content'>
+        <div className='content'>Questions in DB: {String(questionsCount)}</div>
+        <div className='content'>Last updated: {lastUpdatedDate}</div>
         {/* <div className='content'>Registered users: {String(questionsCount)}</div> */}
+        <div className='content'>
+          <a href='https://forms.gle/hm8TUuyHYk4TAbqK8' target='_blank'>
+            Subscribe
+          </a>
+          for new questions
+        </div>
       </div>
     </div>
   );
